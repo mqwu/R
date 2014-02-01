9337ac171c551bebe666644e469acf003186f847
mqwu <send2mqwu@gmail.com>
2014-02-01 16:08:42 -0600

    Add: 1. How to change R default lib path 2. How to change RStudio packages source

---
+++ note.R
@@ -0,0 +1,14 @@
+
+ ### R Startup Setting
+ 
+ ## Change default library path
+  # 1. Create an “Renviron.site” file under ..R/etc/
+  # 2. set R_LIBS as the desired lib path, 
+  #    e.g. R_LIBS=C:/Apps/software/R-3.0.2/library
+
+
+ ### RStudio Setting
+ 
+ ## Change Repositories (Packages source)
+  # 1. chooseCRANmirror() 
+  # 2. pick one...
