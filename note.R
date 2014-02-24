440d7c9c8aa2b68298629f676665ea80a6234eb8
mqwu <send2mqwu@gmail.com>
2014-02-24 10:40:15 -0600

    Shiny
    Deploy two application in one folder

--- note.R
+++ note.R
@@ -22,3 +22,38 @@
  # delete row 5 and 7
  ind=c(5,7)
  d = a[-ind, ]
+
+ 
+ ### Shiny Package
+ 
+ ## Deployment tips
+ 
+ # Deploy two application in one folder
+ 
+ # 1. Put source code (ui.R, server.R) in 
+ #    ./appfolder/src/app1
+ #    ./appfolder/src/app2
+ 
+ # 2. Create two run files (run1.R, run2.R) in  
+ #    ./appfolder/src/
+ #    with code below
+  .libPaths(unique(c("./lib",.libPaths())))
+  library(shiny)
+ 
+  if (file.exists ("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")) {
+    options(browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
+  } else {
+    options(browser = "C:/Program Files/Google/Chrome/Application/chrome.exe")
+  }
+
+  runApp('C:/Apps/projects/TED/SampleSize/src/app1', launch.browser=TRUE) # run1.R
+ #runApp('C:/Apps/projects/TED/SampleSize/src/app2', launch.browser=TRUE) # run2.R
+ 
+ # 3. Creat R shell short cut under ./appfolder/ and modify its properties (e.g. app1)
+ # Target: "C:\Program Files (x86)\R\R-3.0.2\bin\i386\Rterm.exe" --file="run1.R"
+ # Start in: .\appfolder\src\
+ 
+ 
+ 
+ 
+ 
