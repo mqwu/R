cc1cb6d5ec06db8220cefcb6369e05e86b9ffcb7
mqwu <send2mqwu@gmail.com>
2017-02-21 14:20:02 -0600

    Update project_template.R

--- project_template.R
+++ project_template.R
@@ -53,19 +53,23 @@ plot_box(d, x="catvar", y="vname")
 #--------------------------------------------------------
 # Tools (should be sourced first before the above procedure)
 #-------------------------------------------------------- 
-plot_HistDensity <- function(x){
+plot_HistDensity <- function(x, title){
   # plot histogram with density curve overlaid
   # Arg:
   #   x: numeric vector 
   # Return:
   #   plot of histogram with density curve overlaid
   x <- x[!is.na(x)]  # rm NA records
-  hist(x, prob=TRUE, col="grey", nclass=50) # prob=TRUE for probabilities not counts
+  hist(x, prob=TRUE, col="grey", nclass=50, main=NA) # prob=TRUE for probabilities not counts
   lines(density(x, adjust=1), col="blue", lty="solid", lwd=2) # add a density est
+  title(main=title)
+  
+  dev.copy(png,paste0(title,"_hist.png"))
+  dev.off()
 }
 
 
-plot_Box <- function(d, x="1", y){
+plot_Box <- function(d, x="1", y, title=""){
   # plot boxplot
   # Arg:        
   #   d: data frame
@@ -73,22 +77,24 @@ plot_Box <- function(d, x="1", y){
   #   y: numerical var
   # return: 
   #   box plot of y catergoried by x
-  
   p <- d %>%  
-          filter_(!is.na(y)) %>%  # rm NA records
-          ggplot(., aes_string(x=x, y=y)) +
-          geom_boxplot(aes_string(fill=x))
-        
+    filter_(!is.na(y)) %>%  # rm NA records
+    ggplot(., aes_string(x=x, y=y)) +
+    geom_boxplot(aes_string(fill=x)) + 
+    ggtitle(title)
+  
   if(x=="1"){ # categorical var with 1 level
     p + theme(axis.title.x=element_blank(),
               axis.text.x=element_blank(),
               axis.ticks.x=element_blank(),
+              plot.title = element_text(hjust = 0.5),
               legend.position="none")
   } else { # categorical var with multiple level
     p
   }
+  ggsave(paste0(title,"_box.png"))
 }
-  
+
 
 
 
