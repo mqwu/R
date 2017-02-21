a859712acbdc0a83f316e0ec16c452416b8c5562
mqwu <send2mqwu@gmail.com>
2017-02-21 11:14:43 -0600

    Update project_template.R

--- project_template.R
+++ project_template.R
@@ -26,18 +26,73 @@ d <- read.csv(b_path, header=TRUE, na.strings="N/A")
 colnames(d) <- gsub("\\.+", ".", colnames(d))
 colnames(d) <- gsub("\\.$", "", colnames(d))
 
-# select vars
+# select vars and filter obs
 d <- d %>% 
         select(varA:varB) %>%
         filter(varC=="Y")
+
       
 
 #--------------------------------------------------------
 # EDA
 #-------------------------------------------------------- 
+## Numerical vars
+# hist
+plot_HistDensity(d$v1)
 
+# boxplot
+plot_box(d, x="catvar", y="vname")
 
 
 #--------------------------------------------------------
 # Modelling
 #-------------------------------------------------------- 
+
+
+
+#--------------------------------------------------------
+# Tools (should be sourced first before the above procedure)
+#-------------------------------------------------------- 
+plot_HistDensity <- function(x){
+  # plot histogram with density curve overlaid
+  # Arg:
+  #   x: numeric vector 
+  # Return:
+  #   plot of histogram with density curve overlaid
+  x <- x[!is.na(x)]  # rm NA records
+  hist(x, prob=TRUE, col="grey", nclass=50) # prob=TRUE for probabilities not counts
+  lines(density(x, adjust=1), col="blue", lty="solid", lwd=2) # add a density est
+}
+
+
+plot_Box <- function(d, x="1", y){
+  # plot boxplot
+  # Arg:        
+  #   d: data frame
+  #   x: catergorical var 
+  #   y: numerical var
+  # return: 
+  #   box plot of y catergoried by x
+  
+  p <- d %>%  
+          filter_(!is.na(y)) %>%  # rm NA records
+          ggplot(., aes_string(x=x, y=y)) +
+          geom_boxplot(aes_string(fill=x))
+        
+  if(x=="1"){ # categorical var with 1 level
+    p + theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+              legend.position="none")
+  } else { # categorical var with multiple level
+    p
+  }
+}
+  
+
+
+
+
+
+
+
