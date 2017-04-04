ba67b7bd8bfb6bb8cac06f13765fb70048f809a7
mqwu <send2mqwu@gmail.com>
2017-04-04 15:36:43 -0500

    load_libs

--- tools.R
+++ tools.R
@@ -2,6 +2,22 @@
 #--------------------------------------------------------
 # Tools: collection of useful functions
 #-------------------------------------------------------- 
+load_libs <- function(requiredPackages) {
+  # load all required libs, if they are not installed try to install them automatically
+  #
+  # Args:
+  #  requiredPackages: List of strings with package names that are about to be loaded 
+  #                    If they are not installed automated installation is attempted
+  missingPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[, "Package"])]
+  if (length(missingPackages)) {
+    install.packages(missingPackages, dependencies = TRUE)
+  }
+
+  for (package in requiredPackages) {
+    library(package, character.only = TRUE)
+  }
+}
+
 plot_HistDensity <- function(x, title){
   # plot histogram with density curve overlaid
   # Arg:
