1068af45031bb05a6cc275f984f94a724d98f9d9
mqwu <send2mqwu@gmail.com>
2017-09-17 00:43:29 -0500

    Update note.R

--- note.R
+++ note.R
@@ -991,4 +991,20 @@ pwalk(list(x=sims, breaks=nice_breaks, main=nice_titles), hist, xlab = "")
 sims %>%
   walk(hist) %>%
   map(summary)
+ 
        
+## hidden argument
+# Read in the swimming_pools.csv to pools
+pools <- read.csv("swimming_pools.csv")
+
+# Examine the structure of pools
+str(pools)
+
+# Change the global stringsAsFactor option to FALSE
+options(stringsAsFactors = FALSE)
+
+# Read in the swimming_pools.csv to pools2
+pools2 <- read.csv("swimming_pools.csv")
+
+# Examine the structure of pools2
+str(pools2)       
