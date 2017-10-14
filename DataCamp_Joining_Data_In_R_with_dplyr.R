20007911a004d7c6a6312b734f6ba858777d9310
mqwu <send2mqwu@gmail.com>
2017-10-14 17:04:12 -0500

    Update DataCamp_Joining_Data_In_R_with_dplyr.R

--- DataCamp_Joining_Data_In_R_with_dplyr.R
+++ DataCamp_Joining_Data_In_R_with_dplyr.R
@@ -3,7 +3,8 @@
 #----------------------------------------------------------
 # dplyr preserve order of rows in your raw data
 
-## mutating join, add one df to another df
+## mutating joins
+#add one df to another df
 # left_join: keep every row of primary df, and add cols of augment df with matching rows
 left_join(df_primary, df_augment, by = "key")
 left_join(df_primary, df_augment, by = c("key1", "key2"))
@@ -42,5 +43,33 @@ union(df1, df2)
 intersect(df1, df2)
 setdiff(df1, df2)  # return rows only appear in df1
 
-# Check two sets
-setequal(bands2, bands3)
+aerosmith %>% 
+  # Create the new dataset using a set operation
+  union(greatest_hits) %>% 
+  # Count the total number of songs
+  nrow()
+
+# compare sets
+setequal(df1, df2)  # regardless of order of rows
+identical(df1, df2)  # considering order of rows
+
+
+## bind dataset
+# base
+rbind()
+cbind()
+
+# dplyr
+# faster, return a tibble, can handle list of dfs, .id argument
+bind_rows()
+bind_cols()  # not matching any key!! physical bind
+
+bind_rows(drug_a=df.a, drug_b=df.b, .id = "drug")
+
+jimi %>%  # jimi have 3 lists, each one is a df
+  # Bind jimi into a single data frame
+  bind_rows(.id="album") %>% 
+  # Make a complete data frame
+  left_join(discography, by = "album" )
+
+
