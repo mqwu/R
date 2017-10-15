e20e16027ba2c4ed41db2e55d093c78a8ac47bf8
mqwu <send2mqwu@gmail.com>
2017-10-15 00:10:38 -0500

    Update DataCamp_Joining_Data_In_R_with_dplyr.R

--- DataCamp_Joining_Data_In_R_with_dplyr.R
+++ DataCamp_Joining_Data_In_R_with_dplyr.R
@@ -3,19 +3,24 @@
 #----------------------------------------------------------
 # dplyr preserve order of rows in your raw data
 
-## mutating joins
+### mutating joins
 #add one df to another df
 # left_join: keep every row of primary df, and add cols of augment df with matching rows
 left_join(df_primary, df_augment, by = "key")
 left_join(df_primary, df_augment, by = c("key1", "key2"))
+merge(df_primary, df_augment, by = "key", all.x=T, all.y=F)
+
 
 right_join(df_augment, df_primary, by = "key")
+merge(df_primary, df_augment, by = "key", all.x=F, all.y=T)
 
 # inner_join: return rows appear in both df (exclusive join)
 inner_join(df1, df2, by = "key")
+merge(df_primary, df_augment, by = "key", all=F)
 
 # full_join: return every rows appear in either df (inclusive join)
 full_join(df1, df2, by = "key")
+merge(df_primary, df_augment, by = "key", all=T)
 
 # %>%
 bands %>% 
@@ -24,7 +29,7 @@ bands %>%
   select(first, last, band)
 
 
-## filter join
+### filter join
 # semi_join: filtering primary df with rows that the key appears in the 2nd df
 semi_join(df1, df2, by = "name")  # only keep df1 cols and keep rows that appear in df2
 
@@ -38,7 +43,7 @@ albums %>%
 anti_join(df1, df2, by = "key")  # only keep df1 cols and keep rows that does not appear in df2
 
 
-## set operations
+### set operations
 union(df1, df2)
 intersect(df1, df2)
 setdiff(df1, df2)  # return rows only appear in df1
@@ -53,7 +58,7 @@ aerosmith %>%
 setequal(df1, df2)  # regardless of order of rows
 identical(df1, df2)  # considering order of rows
 
-
+### assembling data
 ## bind dataset
 # base
 rbind()
@@ -72,4 +77,65 @@ jimi %>%  # jimi have 3 lists, each one is a df
   # Make a complete data frame
   left_join(discography, by = "album" )
 
+data_frame()  # vs. data.frame()
+# will not coerces strings to factors.
+# will not change your column names
+# never adds row names
+# evaluates its arguments lazily and in order.
+# Make combined data frame using data_frame()
+data_frame(year = hank_year, song = hank_song, peak = hank_peak) %>% 
+    filter(peak == 1)
 
+as_data_frame()
+# Convert the hank list into a data frame
+as_data_frame(hank) %>% 
+    filter(peak == 1)
+
+
+## data type 6
+typeof()
+logical
+character
+integer
+double
+complex
+raw
+
+## class of data for catergorical data (2nd attribute)
+factor
+# Factors are stored as integers, 
+# and have labels associated with these unique integers. 
+# While factors look (and often behave) like character vectors, they are actually integers under the hood, 
+# and you need to be careful when treating them like strings.
+y <- factor(c(5, 6, 7, 6)
+unclass(y)
+as.character(y)  # shows the level of y
+as.numeric(y)  # shows the integer representaion of y
+as.numeric(as.character(y))  # two step process to convert factor to numeric!!    
+
+# dplyr for combining different type
+# coerce two factors to a string if the factors have different levels.
+# coerce two factors to a string if the factors have the same levels in a different order.
+# return an error instead of coercing logicals, integers, and numerics to a character.            
+            
+### advance join
+# missing key values
+filter(!is.na(key))
+
+# missing key cols (sometimes key is the row names)
+rownames_to_column(df, var = "name")
+
+# duplicate key values
+# will have duplicate mathes
+
+# mismatched keys
+left_join(df1, df2, by = c("name1"="name2"))
+            
+# conflicting vars
+left_join(df1, df2, by = c("name"), suffix = c("1", "2"))
+            
+### join multiple tables
+library(purrr)
+lists <- list(df1, df2, df3)
+reduce(lists, left_join, by = "name")  # apply a function in an iterative fashion to many datasets
+            
