4cf1c6cb688002739d7e173055be339b63c743f1
mqwu <send2mqwu@gmail.com>
2017-10-18 14:39:08 -0500

    Update DataCamp_string_manipulation_with_stringr.R

--- DataCamp_string_manipulation_with_stringr.R
+++ DataCamp_string_manipulation_with_stringr.R
@@ -3,6 +3,12 @@
 # Stirng Manipulation with stringr (DataCamp course)
 #----------------------------------------------------------
 
+# " inside quote
 "hi!"  # normal
-' "hi!" ' # have ""
-" I'd say \"hi!\" "  # have ' and ""
+' "hi!" ' # have "" inside quote, use ' '
+" I'd say \"hi!\" "  # have both ' and "" inside quote, use \" inside quote
+
+writeLines(string) # to see the string it represents (do not use print, it will shows \")
+writeLines("hello\n\U1F30D")
+writeLines("hello\\")  # escape \ for \
+
