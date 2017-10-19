282055666453020fdcfb2503e18940fc01a3d840
mqwu <send2mqwu@gmail.com>
2017-10-19 13:36:32 -0500

    Update DataCamp_string_manipulation_with_stringr.R

--- DataCamp_string_manipulation_with_stringr.R
+++ DataCamp_string_manipulation_with_stringr.R
@@ -34,3 +34,36 @@ table(boy_last_letter)
 # Extract the first letter in girl_names, then tabulate
 girl_first_letter <- str_sub(girl_names, 1,1)
 table(girl_first_letter)
+
+
+## hunting for matches
+str_detect(string=a, pattern="yy"))
+str_subset(string=a, pattern=fixed("xx"))
+str_extract(string, pattern)  # 1. return the same length as string, 2. only return patterns
+str_count(string=a, pattern=fixed("xx"))
+
+
+# Look for pattern "zz" in boy_names
+contains_zz <- str_detect(string=boy_names, pattern=fixed("zz"))
+# Examine str() of contains_zz
+str(contains_zz)
+# How many names contain "zz"?
+sum(contains_zz)
+# Which names contain "zz"?
+boy_names[contains_zz]
+
+# Find girl_names that contain "U" and "z"
+str_subset(str_subset(girl_names, "U"), "z")
+
+
+# Count occurrences of "a" in girl_names
+number_as <- str_count(girl_names, "a")
+# Count occurrences of "A" in girl_names
+number_As <- str_count(girl_names, "A")
+# Histograms of number_as and number_As
+hist(number_as)
+hist(number_As)
+# Find total "a" + "A"
+total_as <- number_as + number_As
+# girl_names with more than 4 a's
+girl_names[total_as>4]
