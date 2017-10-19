31a0934a4a74c475523d05d4ca6b332ec6e950b6
mqwu <send2mqwu@gmail.com>
2017-10-19 09:49:04 -0500

    Update DataCamp_string_manipulation_with_stringr.R

--- DataCamp_string_manipulation_with_stringr.R
+++ DataCamp_string_manipulation_with_stringr.R
@@ -1,4 +1,3 @@
-
 #----------------------------------------------------------
 # Stirng Manipulation with stringr (DataCamp course)
 #----------------------------------------------------------
@@ -12,3 +11,26 @@ writeLines(string) # to see the string it represents (do not use print, it will 
 writeLines("hello\n\U1F30D")
 writeLines("hello\\")  # escape \ for \
 
+
+library(stringr)
+# most fun starts with str_
+
+str_c()  
+# similar to paste0, concatenate, default sep="" (not space)
+# str_c() propagates missing values. That means combining any strings with a missing value will result in another missing value.
+str_c(my_toppings_str, collapse=",")  # one NA will make entire results NA when collapse
+
+
+str_length()
+# Confirm str_length() works with factors
+head(str_length(factor(boy_names)))
+
+
+str_sub()
+# Extract the last letter in boy_names, then tabulate
+boy_last_letter <- str_sub(boy_names,-1, -1)
+table(boy_last_letter)
+
+# Extract the first letter in girl_names, then tabulate
+girl_first_letter <- str_sub(girl_names, 1,1)
+table(girl_first_letter)
