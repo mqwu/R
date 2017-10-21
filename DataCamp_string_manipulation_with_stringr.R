8d897eb75993461f2a4ad472a0460b02137a1843
mqwu <send2mqwu@gmail.com>
2017-10-21 15:22:39 -0500

    Update DataCamp_string_manipulation_with_stringr.R

--- DataCamp_string_manipulation_with_stringr.R
+++ DataCamp_string_manipulation_with_stringr.R
@@ -11,6 +11,8 @@ writeLines(string) # to see the string it represents (do not use print, it will 
 writeLines("hello\n\U1F30D")
 writeLines("hello\\")  # escape \ for \
 
+# examples
+https://cran.r-project.org/web/packages/stringr/vignettes/stringr.html
 
 library(stringr)
 # most fun starts with str_
@@ -97,3 +99,60 @@ word_lengths <- lapply(words, str_length)
   
 # Average word length per line
 lapply(word_lengths, mean)
+
+
+str_replace(str, pattern, replacement)
+str_replace_all(str, pattern, replacement)
+
+
+ids <- c("ID#: 192", "ID#: 118", "ID#: 001")
+
+# Replace "ID#: " with ""
+id_nums <- str_replace(ids, "ID#: ", "")
+
+# Turn id_nums into numbers
+id_ints <- as.numeric(id_nums)
+  
+# Some (fake) phone numbers
+phone_numbers <- c("510-555-0123", "541-555-0167")
+
+# Use str_replace() to replace "-" with " "
+str_replace(phone_numbers, "-", " ")
+
+# Use str_replace_all() to replace "-" with " "
+str_replace_all(phone_numbers, "-", " ")
+
+# Turn phone numbers into the format xxx.xxx.xxxx
+str_replace_all(phone_numbers, "-", ".")
+
+
+# --- Task 1 ----
+# Define some full names
+names <- c("Diana Prince", "Clark Kent")
+
+# Split into first and last names
+names_split <- str_split(names, " ", simplify=TRUE)
+
+# Extract the first letter in the first name
+abb_first <- str_sub(names_split[,1], 1,1)
+
+# Combine the first letter ". " and last name
+str_c(str_c(abb_first, ". "), names_split[,2])
+
+# --- Task 2 ----
+# Use all names in babynames_2014
+all_names <- babynames_2014$name
+
+# Get the last two letters of all_names
+last_two_letters <- str_sub(all_names, -2, -1)
+
+# Does the name end in "ee"?
+ends_in_ee <- str_detect(last_two_letters, pattern=fixed("ee"))
+
+# Extract rows and "sex" column
+sex <- babynames_2014[ends_in_ee,]$sex
+
+# Display result as a table
+table(sex)
+
+
