e63271557fa40bfeedcd4fa5eb148241fcd7bc3d
mqwu <send2mqwu@gmail.com>
2017-10-22 01:03:44 -0500

    Update DataCamp_string_manipulation_with_stringr.R

--- DataCamp_string_manipulation_with_stringr.R
+++ DataCamp_string_manipulation_with_stringr.R
@@ -155,4 +155,130 @@ sex <- babynames_2014[ends_in_ee,]$sex
 # Display result as a table
 table(sex)
 
+## Regular expression
+library(rebus) # build Regular Expressions in a Human Readable Way
+library(stringr)
 
+# Some strings to practice with
+x <- c("cat", "coat", "scotland", "tic toc")
+
+# Run me
+str_view(x, pattern = START %R% "c") # start of string %R% = then
+
+# Match the strings that start with "co" 
+str_view(x, pattern = START %R% "co")  # help you check results
+
+# Match the strings that end with "at"
+str_view(x, pattern =  "at" %R% END)
+
+# Match the strings that is exactly "cat"
+str_view(x, pattern = START %R% "cat" %R% END)  # end of string
+
+## Wild card
+# any char
+ANY_CHAR
+x <- c("cat", "coat", "scotland", "tic toc")
+
+# Match any character followed by a "t"
+str_view(x, pattern = ANY_CHAR %R% "t")
+
+# Match a "t" followed by any character
+str_view(x, pattern = "t" %R% ANY_CHAR)
+
+# Match two characters
+str_view(x, pattern = ANY_CHAR %R% ANY_CHAR)
+
+# Match a string with exactly three characters
+str_view(x, pattern = START %R% ANY_CHAR %R% ANY_CHAR %R% ANY_CHAR %R% END)
+
+# q followed by any character
+pattern <- "q" %R% ANY_CHAR
+
+# Test pattern 
+str_view(c("Quentin", "Kaliq", "Jacques",  "Jacqes"), pattern)  
+
+# Find names that have the pattern
+names_with_q <- str_subset(boy_names, pattern)
+length(names_with_q)
+
+# Find part of name that matches pattern
+part_with_q <- str_extract(boy_names, pattern)
+table(part_with_q)
+
+# Did any names have the pattern more than once?
+count_of_q <- str_count(boy_names, pattern)
+table(count_of_q)
+# Which babies got these names?
+with_q <- str_detect(boy_names, pattern)
+
+# What fraction of babies got these names?
+mean(with_q)
+
+## alternative 
+or(A, B)
+
+# Match Jeffrey or Geoffrey
+whole_names <- or("Jeffrey", "Geoffrey")
+str_view(boy_names, pattern = whole_names, 
+  match = TRUE)
+
+# Match Jeffrey or Geoffrey, another way
+common_ending <- or("Je", "Geo") %R% "ffrey"
+str_view(boy_names, pattern = common_ending, 
+  match = TRUE)
+
+# Match with alternate endings
+by_parts <- or("Je", "Geo") %R% "ff" %R% or("ry", "ery", "rey", "erey")
+str_view(boy_names, 
+  pattern = by_parts, 
+  match = TRUE)
+
+# Match names that start with Cath or Kath
+ckath <- START %R% or("Cath", "Kath")
+str_view(girl_names, pattern = ckath, match = TRUE)
+
+
+## char classes
+"match one (and only one) of the following characters"
+char_class()
+"any single character that isn't one of the following"
+
+# Create character class containing vowels
+vowels <- char_class("aeiouAEIOU")
+
+# Print vowels
+print(vowels)
+
+# See vowels in x with str_view()
+str_view(x, vowels)
+
+# See vowels in x with str_view_all()
+str_view_all(x, vowels)
+
+# Number of vowels in boy_names
+num_vowels <- str_count(boy_names, vowels)
+mean(num_vowels)
+
+# Proportion of vowels in boy_names
+name_length <- str_length(boy_names)
+mean(num_vowels/name_length)
+
+#The rebus functions one_or_more(), zero_or_more() and optional() can be used to wrap parts of a regular expression 
+#to allow a pattern to match a variable number of times.
+negated_char_class()
+
+# Vowels from last exercise
+vowels <- char_class("aeiouAEIOU")
+
+# Use `negated_char_class()` for everything but vowels
+not_vowels <- negated_char_class("aeiouAEIOU")
+
+# See names with only vowels
+str_view(boy_names, 
+  pattern = START %R% one_or_more(vowels) %R% END, 
+  match = TRUE)
+
+# See names with no vowels
+str_view(boy_names, 
+  pattern = START %R% one_or_more(not_vowels) %R% END, 
+  match = TRUE)
